3f3a810fca227c09b6a9c1bd7b972ff55a49ce1f
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2025-05-16 16:44:22 +0900

    feat: add workspaces to package.json and update pnpm-lock.yaml for storybook

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@repo/ui/button";
 export default function Home() {
   return (
     <div>
+      <h1>Minu Kim</h1>
       <Button appName="test open plan assignment">Click me</Button>
     </div>
   );
