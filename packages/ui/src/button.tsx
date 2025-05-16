9e76d51efbd106775f11bac6c717fcaf213af52f
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2025-05-16 16:31:29 +0900

    feat: remove ui components add layout metadata

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -4,14 +4,12 @@ import { ReactNode } from "react";
 
 interface ButtonProps {
   children: ReactNode;
-  className?: string;
   appName: string;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, appName }: ButtonProps) => {
   return (
     <button
-      className={className}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
