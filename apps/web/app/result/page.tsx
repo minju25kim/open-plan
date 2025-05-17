11865b8eb4f96e3938bfd19b048214482d91a1d0
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2025-05-17 16:17:06 +0900

    feat(page): add result page for example

---
+++ apps/web/app/result/page.tsx
@@ -0,0 +1,14 @@
+import type { JSX } from 'react';
+import { Button } from '@repo/ui/button';
+
+export default function Home(): JSX.Element {
+
+    return (
+        <div>
+            why?
+            <Button style={{ width: 335, margin: '20px 40px' }}>
+                다음
+            </Button>
+        </div>
+    );
+}
