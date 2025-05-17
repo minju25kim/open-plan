9d2970de62ebdd0d5c3dafeb388ed9a705650bc6
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2025-05-17 14:44:49 +0900

    Delete apps/storybook/storybook-static directory

--- apps/storybook/storybook-static/assets/Button.stories-C8OUQAY0.js
+++
@@ -1,6 +0,0 @@
-import{j as p}from"./jsx-runtime-D_zvdyIk.js";const s=({children:n,appName:a})=>p.jsx("button",{onClick:()=>alert(`Hello from your ${a} app!`),children:n});s.__docgenInfo={description:"",methods:[],displayName:"Button",props:{children:{required:!0,tsType:{name:"ReactNode"},description:""},appName:{required:!0,tsType:{name:"string"},description:""}}};const c={title:"Assignment/Button",component:s,parameters:{layout:"centered"},tags:["autodocs"]},e={args:{children:"Demo Button",appName:"storybook"}};var t,o,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    children: 'Demo Button',
-    appName: 'storybook'
-  }
-}`,...(r=(o=e.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const d=["Demo"];export{e as Demo,d as __namedExportsOrder,c as default};
